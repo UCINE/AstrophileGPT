46a70fbb74625c5382a470e81e39de74c0d6ccd9
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2023-12-01 03:11:16 +0000

    update

---
+++ script.js
@@ -0,0 +1,31 @@
+const userInput = document.getElementById('user-input');
+userInput.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        let inputText = userInput.value;
+        userInput.value = '';
+        addMessageToChat('You', inputText);
+        getAIResponse(inputText);
+    }
+});
+
+function addMessageToChat(sender, message) {
+    let chatContainer = document.getElementById('chat-container');
+    chatContainer.innerHTML += `<b>${sender}:</b> ${message}<br/>`;
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+
+async function getAIResponse(message) {
+    const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer YOUR_OPENAI_API_KEY'
+        },
+        body: JSON.stringify({
+            prompt: message,
+            max_tokens: 150
+        })
+    });
+    const data = await response.json();
+    addMessageToChat('AI', data.choices[0].text.trim());
+}
